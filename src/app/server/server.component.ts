93dd334987b2cd675d8051a31e27ae0d0005bfd9
pluto-atom-4 <genko2@hotmail.com>
2019-07-16 19:27:07 -0700

    added ngStyle directive along with getColor() function.

--- src/app/server/server.component.ts
+++ src/app/server/server.component.ts
@@ -9,8 +9,15 @@ export class ServerComponent {
   serverId = 10;
   serverStatus = 'offline';
 
+  constructor() {
+    this.serverStatus = Math.random() > 0.5 ? 'online' : 'offline';
+  }
+
   getServerStatus(): string {
     return this.serverStatus;
   }
 
+  getColor() {
+    return this.serverStatus === 'online' ? 'green' : 'red';
+  }
 }
