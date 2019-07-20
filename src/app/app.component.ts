4c0a5bb07874ebaeaf14a92c8f9308058f4158a9
pluto-atom-4 <genko2@hotmail.com>
2019-07-19 20:01:45 -0700

    cleaned up for a new application

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,4 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  showSecret = false;
-  log = [];
-
-
-  onToggleDetails() {
-    this.showSecret = !this.showSecret;
-    this.log.push(new Date());
-  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,14 +4,10 @@ import { NgModule } from '@angular/core';
 import { NgbModule} from '@ng-bootstrap/ng-bootstrap';
 import { AppComponent } from './app.component';
 import { FormsModule } from '@angular/forms';
-import { ServerComponent } from './server/server.component';
-import { ServersComponent } from './servers/servers.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ServerComponent,
-    ServersComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
